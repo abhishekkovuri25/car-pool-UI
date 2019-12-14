d43760f1963a2e075b439b32de2050b290636bc6
kovuriabhishek <abhishek.kovuri@coviam.com>
2019-12-15 00:50:10 +0530

    Made small changes

--- src/views/NewCar/NewCar.vue
+++ src/views/NewCar/NewCar.vue
@@ -5,10 +5,10 @@
                 <h2> Register Your Vehicle </h2>
                 <b-card class="card">
                     <b-row>
-                        Vehicle Name : <b-form-select v-model="carName" :options="options"></b-form-select>
-                        Vehicle Number : <b-form-input id="number" type="number" v-model="carNumber"></b-form-input>
-                        Vehicle Type : <b-form-select v-model="carType"></b-form-select>
-                        Vehicle Color : <b-form-input id="time" v-model="carColor"></b-form-input>
+                        Vehicle Name : <b-form-input v-model="vehicleName"></b-form-input>
+                        Vehicle Number : <b-form-input v-model="vehicleNumber"></b-form-input>
+                        Vehicle Type : <b-form-select v-model="vehicleType" :options="vehicletypes"></b-form-select>
+                        Vehicle Color : <b-form-input id="time" v-model="Color"></b-form-input>
                     </b-row>
                     <b-row align-h="center" class="mt-3">
                         <b-button type="submit" variant="success">Done</b-button>
@@ -18,6 +18,7 @@
         </b-row>
     </b-container>
 </template>
+<script src="./new-car.js"></script>
 <style scoped>
 .card {
     opacity:0.8;
---
+++ src/views/RideListing/RideList.vue
@@ -0,0 +1,7 @@
+<template>
+    <b-container>
+        <b-table striped hover :items="items" :fields="fields"></b-table>
+    </b-container>
+</template>
+<script src="./ride-list.js"></script>
+
